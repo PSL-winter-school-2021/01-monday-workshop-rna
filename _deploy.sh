9f4e9203731c9ebce9c4671decf4ca4574c850c7
Lorette Noiret, Nathalie Lehmann, Eric Van Leen <20380914+LehmannN@users.noreply.github.com>
2021-02-05 00:00:53 +0100

    Clean repository and add rmarkdown files

--- _deploy.sh
+++ _deploy.sh
@@ -5,7 +5,9 @@ commitMessage=$1
 if [ $# -eq 0 ]
 then
 	echo "You must supply a commit message as an argument"
-	echo "Example command: bash _deploy.sh 'Initial commit'"
+	echo "Example command:"
+	echo "   m=\"Your commit message\""
+	echo "   bash _deploy.sh \$m"
 	exit
 fi
 
